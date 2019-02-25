3098c4730b8ffeb1fb5f4b5eb076ff78b0093c90
Mickae Truman <mtruman@nyit.edu>
2019-02-25 02:57:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,9 +82,9 @@ def all_holidays_with_bbq(holiday_hash)
 all_holidays_with_bbq = []
 holiday_hash.each do |season, holiday|
   holiday.each do |holiday_stuff, supply|
-    if supply == "BBQ" 
+    supply.include?("BBQ")
      all_holidays_with_bbq << holiday_stuff
-   end
+   
  end
  end
 end
